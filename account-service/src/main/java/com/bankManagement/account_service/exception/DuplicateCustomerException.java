63805c32341a25f3164c75483c09a09d72e9decb
nikhil-aum <nikhilpatidar.aum@outlook.com>
2026-09-11 19:22:08 +0530

    Ignore AuthControllerTest and CustomerControllerTest files

--- account-service/src/main/java/com/bankManagement/account_service/exception/DuplicateCustomerException.java
+++
@@ -1,7 +0,0 @@
-package com.bankManagement.account_service.exception;
-
-public class DuplicateCustomerException extends RuntimeException{
-    public DuplicateCustomerException(String message) {
-        super(message);
-    }
-}
